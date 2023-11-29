339c0e9de33618248f1ea9e59125f5c6c4590d60
Jordan <jordan.chyehong@gmail.com>
2023-11-29 09:20:16 +0800

    Move constants to Constants.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,13 +11,6 @@ import java.io.FileNotFoundException
 import java.io.IOException
 import java.util.Scanner
 
-private const val INPUT_WORDS_DICTIONARY_FILE_PATH = "resources/words_dictionary.json"
-private const val OUTPUT_WORDS_DICTIONARY_FILE_PATH = "resources/processed_words_dictionary.json"
-private const val DEFAULT_NUMBER_OF_TILES_FIRST_DRAW = 21
-
-// Global variables
-var processedWordMap: MutableMap<String, MutableList<String>> = mutableMapOf()
-
 fun main() {
     val scanner = Scanner(System.`in`)
 
@@ -51,7 +44,7 @@ private fun prepare() {
     } catch (e: JsonSyntaxException) {
         println("Error: Invalid JSON format - ${e.message}")
     }
-    if (stringMap.isEmpty()){
+    if (stringMap.isEmpty()) {
         throw java.lang.Exception("stringMap is empty!")
     }
 
@@ -65,8 +58,8 @@ private fun prepare() {
     // Save to file
     val jsonString = GsonBuilder().setPrettyPrinting().create().toJson(resultMap)
     try {
-        File(OUTPUT_WORDS_DICTIONARY_FILE_PATH).writeText(jsonString)
-        println("Processed words saved to $OUTPUT_WORDS_DICTIONARY_FILE_PATH")
+        File(constants.OUTPUT_WORDS_DICTIONARY_FILE_PATH).writeText(jsonString)
+        println("Processed words saved!")
     } catch (e: IOException) {
         println("Error writing file: ${e.message}")
     } catch (e: Exception) {
@@ -75,19 +68,6 @@ private fun prepare() {
 }
 
 private fun start(gameMode: GameMode) {
-    println("Loading processed words...")
-    try {
-        processedWordMap = loadProcessedWords()
-    } catch (e: FileNotFoundException) {
-        println("Error: File not found - ${e.message}")
-    } catch (e: JsonSyntaxException) {
-        println("Error: Invalid JSON format - ${e.message}")
-    }
-    if (processedWordMap.isEmpty()){
-        throw java.lang.Exception("processedWordMap is empty!")
-    }
-    println("Processed words loaded!")
-
     println("======================================================")
     println("================== GAME IS STARTING ==================")
     println("======================================================")
@@ -98,28 +78,22 @@ private fun start(gameMode: GameMode) {
     val playerPile = Pile(gameMode, mapOf())
 
     println("How many tiles to draw at the start of the game?")
-    val tilesDrawnOnFirstDraw = commonPile.draw(readUserInputInteger() ?: DEFAULT_NUMBER_OF_TILES_FIRST_DRAW)
+    val tilesDrawnOnFirstDraw = commonPile.draw(readUserInputInteger() ?: constants.DEFAULT_NUMBER_OF_TILES_FIRST_DRAW)
     playerPile.add(tilesDrawnOnFirstDraw)
     playerPile.print()
 
+    // test
+    println(playerPile.sortThenCombine())
+    val (longestWord, remainingTiles) = algorithms.findLongestWord(playerPile.sortThenCombine(), mutableListOf())
+    println("longest word formed is $longestWord")
 }
 
 private fun loadRawWords(): Map<String, String> {
     val gson = Gson()
-    val json = File(INPUT_WORDS_DICTIONARY_FILE_PATH).readText(Charsets.UTF_8)
+    val json = File(constants.INPUT_WORDS_DICTIONARY_FILE_PATH).readText(Charsets.UTF_8)
 
     // Use TypeToken to get the correct type for Gson
     val type = object : TypeToken<Map<String, String>>() {}.type
 
     return gson.fromJson(json, type)
-}
-private fun loadProcessedWords(): MutableMap<String, MutableList<String>> {
-    // Read the JSON string from the file
-    val jsonString = File(OUTPUT_WORDS_DICTIONARY_FILE_PATH).readText(Charsets.UTF_8)
-
-    // Gson needs to know the specific generic type of your map
-    val type = object : TypeToken<MutableMap<String, MutableList<String>>>() {}.type
-
-    // Deserialize the JSON string back into a Map
-    return Gson().fromJson(jsonString, type)
 }
---
+++ src/main/kotlin/constants/Constants.kt
@@ -0,0 +1,5 @@
+package constants
+
+const val INPUT_WORDS_DICTIONARY_FILE_PATH = "resources/words_dictionary.json"
+const val OUTPUT_WORDS_DICTIONARY_FILE_PATH = "resources/processed_words_dictionary.json"
+const val DEFAULT_NUMBER_OF_TILES_FIRST_DRAW = 21
