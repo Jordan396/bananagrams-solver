38d9e4d88f85842a8c578522682683ea7427b404
jordan396-tw <jordan.tan@thoughtworks.com>
2024-06-20 18:47:27 +0800

    #2 enhance board test to check contents of system out

--- src/test/kotlin/models/board/BoardTest.kt
+++ src/test/kotlin/models/board/BoardTest.kt
@@ -1,20 +1,44 @@
 package models.board
 
 import models.board.Board
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
+import java.io.ByteArrayOutputStream
+import java.io.OutputStream
+import java.io.PrintStream
+import kotlin.test.assertEquals
 
 @DisplayName("BoardTest")
 class BoardTest {
     @Nested
     @DisplayName("add")
     inner class Add {
+        private val outContent = ByteArrayOutputStream()
+
+        @BeforeEach
+        fun setUpStreams() {
+            System.setOut(PrintStream(outContent))
+        }
+
+        @AfterEach
+        fun restoreStreams() {
+            System.setOut(System.out)
+        }
+
         @Test
-        fun `should add a word to the middle of the board when board is empty`() {
+        fun `should add a word to the board when board is empty`() {
             val board: Board = Board
             board.add("hello")
             board.print()
+
+            assertEquals(
+                "_______\n" +
+                        "|hello|\n" +
+                        "-------", outContent.toString()
+            )
         }
     }
 }
