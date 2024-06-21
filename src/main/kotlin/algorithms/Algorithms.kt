ed72678a7c075d2d356e0c24753e74ba8d5bde13
jordan396-tw <jordan.tan@thoughtworks.com>
2024-06-21 12:37:59 +0800

    modify findLongestWord algorithm to disregard unused tiles

--- src/main/kotlin/algorithms/Algorithms.kt
+++ src/main/kotlin/algorithms/Algorithms.kt
@@ -3,13 +3,13 @@ package algorithms
 import words.ProcessedWordMap
 import java.util.*
 
-fun findLongestWord(wordMap: ProcessedWordMap, input: String): Pair<String?, MutableList<Char>> {
+fun findLongestWord(wordMap: ProcessedWordMap, input: String): String {
     if (input.isEmpty())
-        return Pair(null, mutableListOf())
+        return ""
 
     // initialise with base word
-    val queue: Queue<Pair<String, MutableList<Char>>> = LinkedList()
-    queue.add(Pair(input, mutableListOf()))
+    val queue: Queue<String> = LinkedList()
+    queue.add(input)
 
     // optimisation, track all processed strings
     val set: MutableSet<String> = mutableSetOf(input)
@@ -19,27 +19,23 @@ fun findLongestWord(wordMap: ProcessedWordMap, input: String): Pair<String?, Mut
         val current = queue.peek()
         queue.remove()
 
-        if (current.first.isEmpty()){
-            return Pair(null, input.toMutableList())
+        if (current.isEmpty()){
+            return ""
         }
 
-        if (wordMap.processedWordMap().containsKey(current.first)){
-            return Pair(wordMap.processedWordMap()[current.first]?.get(0), current.second)
+        if (wordMap.processedWordMap().containsKey(current)){
+            return wordMap.processedWordMap()[current]?.get(0)!!
         } else {
-            for (i in current.first.indices){
-                val newString = utils.removeCharAtIndex(current.first, i)
+            for (i in current.indices){
+                val newString = utils.removeCharAtIndex(current, i)
                 if (!set.contains(newString)){
-                    val newString = utils.removeCharAtIndex(current.first, i)
-                    val newUnusedTiles = current.second.toMutableList()
-                    newUnusedTiles.add(current.first[i])
-                    queue.add(Pair(newString, newUnusedTiles))
+                    queue.add(newString)
                     set.add(newString)
                 }
-
             }
         }
     }
 
 
-    return Pair(null, input.toMutableList())
+    return ""
 }
--- src/test/kotlin/algorithms/AlgorithmsTest.kt
+++ src/test/kotlin/algorithms/AlgorithmsTest.kt
@@ -34,32 +34,28 @@ class AlgorithmsTest {
     @Test
     fun `findLongestWord should return the first item in the word map that matches input exactly`() {
         val wordMap = ProcessedWordMap(TestProcessedWordLoader(testWordMap));
-        val (longestWord, remainingTiles) = algorithms.findLongestWord(wordMap, "eilnst")
+        val longestWord = findLongestWord(wordMap, "eilnst")
         assertEquals(longestWord, "listen")
-        assertEquals(remainingTiles.size, 0)
     }
 
     @Test
     fun `findLongestWord should return the first item in the word map that matches input`() {
         val wordMap = ProcessedWordMap(TestProcessedWordLoader(testWordMap));
-        val (longestWord, remainingTiles) = algorithms.findLongestWord(wordMap, "eilnstv")
+        val longestWord = findLongestWord(wordMap, "eilnstv")
         assertEquals(longestWord, "listen")
-        assertEquals(remainingTiles, mutableListOf('v'))
     }
 
     @Test
     fun `findLongestWord should return empty string if input is empty`() {
         val wordMap = ProcessedWordMap(TestProcessedWordLoader(testWordMap));
-        val (longestWord, remainingTiles) = algorithms.findLongestWord(wordMap, "")
-        assertEquals(longestWord, null)
-        assertEquals(remainingTiles.size, 0)
+        val longestWord = findLongestWord(wordMap, "")
+        assertEquals(longestWord, "")
     }
 
     @Test
     fun `findLongestWord should return empty string if no matching words`() {
         val wordMap = ProcessedWordMap(TestProcessedWordLoader(testWordMap));
-        val (longestWord, remainingTiles) = algorithms.findLongestWord(wordMap, "lnst")
-        assertEquals(longestWord, null)
-        assertEquals(remainingTiles, mutableListOf('l', 'n', 's', 't'))
+        val longestWord = findLongestWord(wordMap, "lnst")
+        assertEquals(longestWord, "")
     }
 }
